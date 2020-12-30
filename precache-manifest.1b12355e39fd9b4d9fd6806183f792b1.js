af7cbadbabad532c86d038e5ef8d3acf1da9c5f6
haritskoding <haritscoding@gmail.com>
2020-12-31 04:20:36 +0700

    Updates

---
+++ precache-manifest.1b12355e39fd9b4d9fd6806183f792b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406b39f68d9be18b3fc772ff3be69785",
+    "url": "/react-lottie-files/index.html"
+  },
+  {
+    "revision": "74a92ee2900413c9f652",
+    "url": "/react-lottie-files/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "6c7a31b183f28d266fe7",
+    "url": "/react-lottie-files/static/js/2.51df2731.chunk.js"
+  },
+  {
+    "revision": "880cf748f7c0ea17fcd415c486527fef",
+    "url": "/react-lottie-files/static/js/2.51df2731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a92ee2900413c9f652",
+    "url": "/react-lottie-files/static/js/main.064b1196.chunk.js"
+  },
+  {
+    "revision": "50ced97a8e965f275ae3",
+    "url": "/react-lottie-files/static/js/runtime-main.85647296.js"
+  }
+]);
